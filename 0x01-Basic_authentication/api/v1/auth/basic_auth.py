3869faf0e11db11471ab04f59fd60651cfe0c3c9
Nyaguthii-C <nyaguthiigc@gmail.com>
2024-01-25 17:38:44 +0300

    decode base64 auth string

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Class that inherits from Auth"""
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -20,4 +21,23 @@ class BasicAuth(Auth):
         if authorization_header.startswith("Basic "):
             return authorization_header.split(" ")[1]
         else:
+            return None
+
+    def decode_base64_authorization_header(
+                      self, base64_authorization_header: str) -> str:
+        """
+        Args:
+             base64_authorization_header - base64 string
+        Returns:
+             the decoded value of a Base64 string
+        """
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            decoded_bytes = base64.b64decode(base64_authorization_header)
+            decoded_str = decoded_bytes.decode('utf-8')
+            return decoded_str
+        except base64.binascii.Error:
             return None
